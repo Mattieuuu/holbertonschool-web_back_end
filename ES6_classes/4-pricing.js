98457823175c62cd9b988719c2b4409219073ab3
root <root@DESKTOP-BQFNG2B.>
2025-03-28 14:30:20 +0100

    fixed bugs

--- ES6_classes/4-pricing.js
+++ ES6_classes/4-pricing.js
@@ -1,35 +1,52 @@
-import Currency from './3-currency.js';
+import Currency from './3-currency';
 
 export default class Pricing {
   constructor(amount, currency) {
+    if (typeof amount !== 'number') {
+      throw new TypeError('Amount must be a number');
+    }
+    if (!(currency instanceof Currency)) {
+      throw new TypeError('Currency must be an instance of the Currency class');
+    }
+
     this._amount = amount;
     this._currency = currency;
   }
 
+  // Getter and setter for 'amount'
   get amount() {
     return this._amount;
   }
 
   set amount(value) {
+    if (typeof value !== 'number') {
+      throw new TypeError('Amount must be a number');
+    }
     this._amount = value;
   }
 
+  // Getter and setter for 'currency'
   get currency() {
     return this._currency;
   }
 
   set currency(value) {
     if (!(value instanceof Currency)) {
-      throw new TypeError('currency must be a Currency instance');
+      throw new TypeError('Currency must be an instance of the Currency class');
     }
     this._currency = value;
   }
 
+  // Method to display full price in the format: amount currency_name (currency_code)
   displayFullPrice() {
     return `${this._amount} ${this._currency.name} (${this._currency.code})`;
   }
 
+  // Static method to convert the price using a conversion rate
   static convertPrice(amount, conversionRate) {
+    if (typeof amount !== 'number' || typeof conversionRate !== 'number') {
+      throw new TypeError('Both amount and conversionRate must be numbers');
+    }
     return amount * conversionRate;
   }
 }
--- ES6_classes/5-building.js
+++ ES6_classes/5-building.js
@@ -1,13 +1,15 @@
 export default class Building {
-  constructor(sqft) {
-    if (this.constructor !== Building 
-      && typeof this.evacuationWarningMessage !== 'function') {
-      throw new Error('Class extending Building must override evacuationWarningMessage');
+    constructor(sqft) {
+      this._sqft = sqft;
+  
+      // Ensure that subclasses implement evacuationWarningMessage
+      if (new.target !== Building && this.evacuationWarningMessage === undefined) {
+        throw new Error('Class extending Building must override evacuationWarningMessage');
+      }
     }
-    this._sqft = sqft;
+  
+    // Getter for sqft
+    get sqft() {
+      return this._sqft;
+    }
   }
-
-  get sqft() {
-    return this._sqft;
-  }
-}
