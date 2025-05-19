aa06971616176c6d3d35e3d44a064d12b69b4224
Mattieuuu <mattieu.mx@gmail.com>
2025-05-19 12:04:07 +0200

    push

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,10 +1,10 @@
-process.stdout.write('Welcome to Holberton School, what is your name?\n');
+process.stdout.write('Welcome to Holberton School, what is your name?\r');
 
 process.stdin.on('data', (data) => {
   const name = data.toString().trim();
-  process.stdout.write(`Your name is: ${name}\n`);
+  process.stdout.write(`Your name is: ${name}\r`);
 });
 
 process.stdin.on('end', () => {
-  process.stdout.write('This important software is now closing\n');
+  process.stdout.write('This important software is now closing\r');
 });
