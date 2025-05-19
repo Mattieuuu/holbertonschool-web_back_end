a1cfb2905d633a651bb4e29aeed498badd864dee
Mattieuuu <mattieu.mx@gmail.com>
2025-05-19 13:57:57 +0200

    push

--- Node_JS_basic/2-read_file.js
+++ Node_JS_basic/2-read_file.js
@@ -2,19 +2,15 @@ const fs = require('fs');
 
 function countStudents(path) {
   try {
-    // Lire le fichier
     const data = fs.readFileSync(path, 'utf8');
-    
-    // Séparer les lignes et enlever les lignes vides
-    const lines = data.toString().split('\n').filter(Boolean);
-    
-    // Enlever l'en-tête et garder que les étudiants
+    const lines = data
+      .toString()
+      .split('\n')
+      .filter((line) => line.trim().length > 0);
     const students = lines.slice(1);
-    
-    // Afficher le nombre total d'étudiants
+
     console.log(`Number of students: ${students.length}`);
-    
-    // Organiser les étudiants par field
+
     const studentsByField = {};
     students.forEach((student) => {
       const [firstName, , , field] = student.split(',');
@@ -23,12 +19,10 @@ function countStudents(path) {
       }
       studentsByField[field].push(firstName);
     });
-    
-    // Afficher les résultats pour chaque field
+
     Object.keys(studentsByField).forEach((field) => {
       console.log(
-        `Number of students in ${field}: ${studentsByField[field].length}. ` +
-        `List: ${studentsByField[field].join(', ')}`
+        `Number of students in ${field}: ${studentsByField[field].length}. List: ${studentsByField[field].join(', ')}`
       );
     });
   } catch (error) {
