9a3559cf13a1edd94c6fcb52335052d737f6dbad
Mattieuuu <mattieu.mx@gmail.com>
2026-03-06 23:18:17 +0100

    push algo

---
+++ .history/Session_authentication/api/v1/auth/session_auth_20260306231649.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+""" SessionAuth module for API authentication
+"""
+from api.v1.auth.auth import Auth
+
+
+class SessionAuth(Auth):
+    """ SessionAuth class to manage session-based authentication
+    """
+    pass
