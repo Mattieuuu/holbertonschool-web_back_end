9bd096af18e36dd986f7d05fe1109da83d12aac3
Mattieuuu <mattieu.mx@gmail.com>
2026-04-03 09:35:36 +0200

    push algo

---
+++ MySQL_Advanced/0-uniq_users.sql
@@ -0,0 +1,7 @@
+-- Create users table with unique email and primary key id
+-- Ensure table creation succeeds even if it already exists
+CREATE TABLE IF NOT EXISTS users (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    name VARCHAR(255)
+);
