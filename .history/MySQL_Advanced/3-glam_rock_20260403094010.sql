85985185bc3dce56808f5ef6431430334184b28d
Mattieuuu <mattieu.mx@gmail.com>
2026-04-03 09:40:26 +0200

    push algo

---
+++ .history/MySQL_Advanced/3-glam_rock_20260403094010.sql
@@ -0,0 +1,6 @@
+-- List Glam rock bands ranked by longevity up to year 2024
+-- Compute lifespan from formed year to split year, or 2024 if still active
+SELECT band_name, IFNULL(split, 2024) - formed AS lifespan
+FROM metal_bands
+WHERE main_style = 'Glam rock'
+ORDER BY lifespan DESC;
