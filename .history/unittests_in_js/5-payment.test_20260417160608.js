6e9d308b2862481e5c38e2af8da12f3aa1464802
Mattieuuu <mattieu.mx@gmail.com>
2026-04-17 16:06:24 +0200

    push algo

---
+++ .history/unittests_in_js/5-payment.test_20260417160608.js
@@ -0,0 +1,28 @@
+const sinon = require('sinon');
+const sendPaymentRequestToApi = require('./5-payment');
+
+describe('sendPaymentRequestToApi', () => {
+  let consoleSpy;
+
+  beforeEach(() => {
+    consoleSpy = sinon.spy(console, 'log');
+  });
+
+  afterEach(() => {
+    consoleSpy.restore();
+  });
+
+  it('logs The total is: 120 for 100 and 20', () => {
+    sendPaymentRequestToApi(100, 20);
+
+    sinon.assert.calledOnce(consoleSpy);
+    sinon.assert.calledWithExactly(consoleSpy, 'The total is: 120');
+  });
+
+  it('logs The total is: 20 for 10 and 10', () => {
+    sendPaymentRequestToApi(10, 10);
+
+    sinon.assert.calledOnce(consoleSpy);
+    sinon.assert.calledWithExactly(consoleSpy, 'The total is: 20');
+  });
+});
